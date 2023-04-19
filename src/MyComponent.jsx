f707d81d3c8e8e97be4a32ed984b1100f07db170
lamsasa <dh85311@gmail.com>
2023-04-19 09:07:57 +0900

    4/18

---
+++ src/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.scss";
+
+const MyComponent = () => {
+
+    return (
+        <div className="my-component">
+            <h1 className="title">안녕하세요. 리액트 세상에 오신 것을 환영합니다.</h1>
+        </div>
+    );
+}
+
+export default MyComponent;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+//  <React.StrictMode>
     <App />
-  </React.StrictMode>
+//  </React.StrictMode> 막고 해도 된다. 디버그가 어려울 경우!
 );
 
 // If you want to start measuring performance in your app, pass a function
